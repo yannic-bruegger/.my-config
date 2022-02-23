2754b52232ef9c9a31b26b8ed5b4f447fde6f794
Yannic Brügger <yannic@bruegger.dev>
2022-02-23 21:26:59 +0100

    use powerlevel10k theme

--- arch/setup.sh
+++ arch/setup.sh
@@ -9,6 +9,8 @@ sudo rm -rf $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sudo rm -rf $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
+
 rm ~/.zshrc
 ln -sf ~/.my-config/config/.zshrc ~/.zshrc
 
